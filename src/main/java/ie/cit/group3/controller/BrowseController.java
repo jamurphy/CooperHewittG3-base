e271303d76b8cce2269a2b8b6f763dc2c1713671
John Murphy <j.a.murphy@mycit.ie>
2015-05-06 09:38:28 +0100

    Displays a Cultural Heritage Object (with image from url). Gets Cultural Heritage Object by checking the repository for the first object that has a valid image URL (This needs to be changed later to search for specific CH object ids).

--- src/main/java/ie/cit/group3/controller/BrowseController.java
+++ src/main/java/ie/cit/group3/controller/BrowseController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import ie.cit.group3.JdbcTemplateTestApplication;
 import ie.cit.group3.domain.ChObject;
+import ie.cit.group3.entity.Crowdsourcing;
 import ie.cit.group3.domain.Image;
 import ie.cit.group3.domain.Participant;
 import ie.cit.group3.domain.Role;
+import ie.cit.group3.repository.CrowdsourcingRepository;
 import ie.cit.group3.repository.ImageRepository;
 import ie.cit.group3.service.ChObjectService;
 import ie.cit.group3.service.ImageService;
@@ -16,10 +18,14 @@ import ie.cit.group3.service.RolesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+//@RestController
 @RequestMapping("/browse")
 public class BrowseController {
 	
@@ -35,6 +41,39 @@ public class BrowseController {
 	@Autowired
 	ImageRepository images;  //changed to Repository (from Service 5/5/2015...change back once debugged)
 	
+	@Autowired
+	CrowdsourcingRepository crowdsourcingRepository;
+	
+
+	@RequestMapping(value = "/addnew", method = RequestMethod.GET) 
+	public ModelAndView addNewComment() 
+	{     
+		//view = "addnewdescription", "crowdsourcing" is key for model data, model passes empty Crowdsourcing object.
+	return new ModelAndView("addnewdescription", "crowsdsourcing", new Crowdsourcing());  
+	} 
+	
+	@RequestMapping(value = "/addnew", method = RequestMethod.POST)
+	public String displayDescription(@ModelAttribute("crowdsourcing") Crowdsourcing crowdsourcing, ModelMap model) 
+{
+		model.addAttribute("description", crowdsourcing.getDescription());
+//		model.addAttribute("lastname", songwriter.getFirstname());
+//		model.addAttribute("age", songwriter.getAge());	
+		
+		crowdsourcingRepository.save(crowdsourcing);
+		
+//		try {
+//			int id=songwriterDAO.createSongwriterGetID(songwriter.getFirstname(), songwriter.getFirstname(), songwriter.getAge());
+//			model.addAttribute("id", Integer.toString(id));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+
+		return "DisplayCrowdSourcingDescription";
+	}       
+	
+	
+	
+	
 	@RequestMapping(value="/listall/objects", method = RequestMethod.GET) 
 	public String listAll(ModelMap model) {			
 			
@@ -47,34 +86,62 @@ public class BrowseController {
 	public String listObject(ModelMap model) {			
 			
 		//next step to pass chObjectID in as parameter
-		String id = "18094365";
 		
-			ChObject object = chobject.get(id);
-			Image objectimage= new Image();
+	//	String id = "18094365";
+		
+		//Find 1st object that has an image with a URL:
+		int index = 0;
+		int imageindex = 0;
+		List<ChObject> objects = chobject.findAll();
+		List<Image> images1 = images.findAll();
+		for (int j = 0; j< objects.size(); j++)
+		{
+			String chid = objects.get(j).getId();
+			images1 = images.getO(chid);
 			
-			List<Image> objectImages = images.getO(id);
-			int maxwidth = 0;
-
-			if (objectImages != null)
+			if (images1 != null)
 			{
-				for (int i = 0; i < objectImages.size() ; i++)
+				for (int z = 0; z < images1.size(); z++)
 				{
-					if (objectImages.get(i).getUrl() != null)
+					if (images1.get(z).getUrl() != null)	
 					{
-						if (objectImages.get(i).getWidth() > maxwidth)
-						{
-							maxwidth = objectImages.get(i).getWidth();
-							objectimage = objectImages.get(i);
-						}		
+						index = j; //capture index of match between CHObject and Image	
+						imageindex = z;
+						j = objects.size(); //exit for loop
+						z = images1.size();
 					}
 				}
 			}
-			else
-			{
-				System.out.println("No objects!");
-			}
-			
-			System.out.println(objectimage);
+		}
+		ChObject object = objects.get(index);
+		Image objectimage = images1.get(imageindex);
+//		
+//			ChObject object = chobject.get(id);
+//			Image objectimage= new Image();
+//			
+//			List<Image> objectImages = images.getO(id);
+//			int maxwidth = 0;
+//
+//			if (objectImages != null)
+//			{
+//				for (int i = 0; i < objectImages.size() ; i++)
+//				{
+//					if (objectImages.get(i).getUrl() != null)
+//					{
+//						if (objectImages.get(i).getWidth() > maxwidth)
+//						{
+//							maxwidth = objectImages.get(i).getWidth();
+//							objectimage = objectImages.get(i);
+//						}		
+//					}
+//				}
+//			}
+//			else
+//			{
+//				System.out.println("No objects!");
+//			}
+			System.out.println("CH Object info: "+object);
+			System.out.println("Image info: "+objectimage);
 //			System.out.println(allimages.get(10));
 //			System.out.println(allimages.get(12));
 //			System.out.println(objectimage);
